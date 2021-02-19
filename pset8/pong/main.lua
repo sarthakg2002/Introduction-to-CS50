36d635eaa11a3cc2bb0be82a048f6629d93c3d5e
sarthakg2002 <64015080+sarthakg2002@users.noreply.github.com>
2021-02-19 14:18:17 -0500

    Update main.lua

--- pset8/pong/main.lua
+++ pset8/pong/main.lua
@@ -1,41 +1,10 @@
---[[
-    GD50 2018
-    Pong Remake
 
-    pong-12
-    "The Resize Update"
-
-    Author: Colton Ogden
-    cogden@cs50.harvard.edu
-
-    Originally programmed by Atari in 1972. Features two
-    paddles, controlled by players, with the goal of getting
-    the ball past your opponent's edge. First to 10 points wins.
-
-    This version is built to more closely resemble the NES than
-    the original Pong machines or the Atari 2600 in terms of
-    resolution, though in widescreen (16:9) so it looks nicer on 
-    modern systems.
-]]
-
--- push is a library that will allow us to draw our game at a virtual
--- resolution, instead of however large our window is; used to provide
--- a more retro aesthetic
---
--- https://github.com/Ulydev/push
 push = require 'push'
 
--- the "Class" library we're using will allow us to represent anything in
--- our game as code, rather than keeping track of many disparate variables and
--- methods
 Class = require 'class'
 
--- our Paddle class, which stores position and dimensions for each Paddle
--- and the logic for rendering them
 require 'Paddle'
 
--- our Ball class, which isn't much different than a Paddle structure-wise
--- but which will mechanically function very differently
 require 'Ball'
 
 WINDOW_WIDTH = 1280
@@ -44,20 +13,12 @@ WINDOW_HEIGHT = 720
 VIRTUAL_WIDTH = 432
 VIRTUAL_HEIGHT = 243
 
--- speed at which we will move our paddle; multiplied by dt in update
 PADDLE_SPEED = 200
 
---[[
-    Runs when the game first starts up, only once; used to initialize the game.
-]]
 function love.load()
-    
-    -- set love's default filter to "nearest-neighbor", which essentially
-    -- means there will be no filtering of pixels (blurriness), which is
-    -- important for a nice crisp, 2D look
+
     love.graphics.setDefaultFilter('nearest', 'nearest')
 
-    -- set the title of our application window
     love.window.setTitle('Pong')
 
     -- "seed" the RNG so that calls to random are always random
